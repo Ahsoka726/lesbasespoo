028f5eb83389ca61ee58027072293c53dc239d9f
NawelleBedr <bedrouni.nawelle@gmail.com>
2023-02-14 10:19:10 +0100

    commit test

--- index.php
+++ index.php
@@ -3,29 +3,8 @@
     require(__DIR__. '/hero.php');
     require(__DIR__. '/orc.php');
 
-    $joueur = new Hero(2000,0,'Epée normal',250, 'Bouclier',600);
-    $joueur2 = new Orc(500,0);
-
-    while ($a <= 4) {
-        // if ($joueur < $joueur2){
-        //         $joueur2($this->attack());
-        //         $joueur($this->attacked());
-        //     }
-        
-    }
-
+    $player = new Hero(2000,0,'Epée normal',250, 'Bouclier',600);
+    $player2 = new Orc(500,0);
     
 
 ?>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    
-</body>
-</html>
